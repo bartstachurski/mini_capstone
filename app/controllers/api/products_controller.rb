acf91c58a017763852d5a93f6342d69c916a8650
bartstachurski <bart.stachurski@gmail.com>
2019-06-25 20:38:47 -0400

    create routes to show individual products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,14 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render 'all_products.json.jb'
   end
+
+  def hario_v60_ceramic_dripper
+    @product = Product.find_by(name: "Hario v60 Ceramic Dripper")
+    render 'hario_v60_ceramic_dripper.json.jb'
+  end
+
+  def hario_v60_filters
+    @product = Product.find_by(name: "Hario v60 Filters")
+    render 'hario_v60_filters.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     get "/products" => "products#all_products"
+    get "/hario_v60_ceramic_dripper" => "products#hario_v60_ceramic_dripper"
+    get "/hario_v60_filters" => "products#hario_v60_filters"
   end
 end
