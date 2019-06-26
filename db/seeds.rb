0aa65b5f9b88b38d2cc510acb5fcf673e11085b1
bartstachurski <bart.stachurski@gmail.com>
2019-06-25 20:10:42 -0400

    add five products to product database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.create([
+  {name: "Chemex Pourover", price: 50, image_url: "https://www.chemexcoffeemaker.com/media/catalog/product/cache/1/image/485x684/9df78eab33525d08d6e5fb8d27136e95/c/o/coffeemaker-classic-six-detail_6.png", description: "Chemex pourover provides super clean flavors"},
+  {name: "Chemex Filters", price: 10, image_url: "https://www.chemexcoffeemaker.com/media/catalog/product/cache/1/image/485x684/9df78eab33525d08d6e5fb8d27136e95/f/i/filters-prefold-circles-detail.png", description: "Chemex filters for the Chemex pourover"},
+])
