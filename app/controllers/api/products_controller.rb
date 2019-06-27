3c6e86153f9929b44268db06c38a6bd6ebe1b6e6
bartstachurski <bart.stachurski@gmail.com>
2019-06-26 22:12:59 -0400

    create routes to look up any single product using query parameter by name or using segment paramter by name

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -13,4 +13,10 @@ class Api::ProductsController < ApplicationController
     @product = Product.find_by(name: "Hario v60 Filters")
     render 'hario_v60_filters.json.jb'
   end
+
+  def query_lookup
+    product_name = params["name"]
+    @product = Product.find_by(name: product_name)
+    render 'query_lookup.json.jb'
+  end
 end
