602e824de75da4fbd8ebb256d1f844b0ee888e4c
bartstachurski <bart.stachurski@gmail.com>
2019-07-09 19:48:40 -0400

    adds new image model that belongs_to products and adds has_many images to Products model and started creating views that show the image urls

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_09_010956) do
+ActiveRecord::Schema.define(version: 2019_07_09_234153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "images", force: :cascade do |t|
+    t.string "url"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "product_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price", precision: 6, scale: 2
-    t.string "image_url"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "instock"
     t.integer "supplier_id"
+    t.integer "image_id"
   end
 
   create_table "suppliers", force: :cascade do |t|
