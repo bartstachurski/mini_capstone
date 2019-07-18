d857b63b838735701476b247f59eeac5e36d4639
bartstachurski <bart.stachurski@gmail.com>
2019-07-17 22:31:41 -0400

    adds html index and show actions for products index link to individual product not working yet

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
     get '/product_lookup_segment/:name' => 'products#lookup'
     post '/carted_products' => 'carted_products#create'
     get '/carted_products' => 'carted_products#index'
-
   end
+  get '/products' => 'products#index'
+  get '/products/:id' => 'products#show'
 end
